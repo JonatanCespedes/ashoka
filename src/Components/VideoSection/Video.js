89e84437f070a1c78a774bb6518dc370fa70ec1b
JonatanCespedes <retratos.jonatan@gmail.com>
2021-03-20 23:13:33 -0300

    Slide & Video section desktop

--- src/Components/VideoSection/Video.js
+++ src/Components/VideoSection/Video.js
@@ -5,7 +5,8 @@ import './Video.css'
 
 const Video = () => {
     return ( 
-        <section className="video-section">
+        <>
+        <section className="video-section-mobile">
             <h3 className="video-title">
                 <span className="text-white">DIGAMOS SI PARA</span><span className="text-blue"> CAMBIAR LO QUE NO</span>
             </h3>
@@ -18,6 +19,22 @@ const Video = () => {
             </p>
             <Button className="btn-white mb-30 align-center" text="HACÉ CLICK ACÁ"/>
         </section>
+        <section className="video-section-desktop">
+            <div className="video-info">
+                <h3 className="video-title">
+                    <span className="text-white">DIGAMOS SI PARA</span><span className="text-blue"> CAMBIAR LO QUE NO</span>
+                </h3>
+                <hr className="video-line"/>
+                <p>
+                    Buscamos identificar y conectar jóvenes transformadorxs que representen a cada rincón de Argentina. Si querés conocer más sobre Tribu 24
+                </p>
+                <Button className="btn-white mb-30 align-center" text="HACÉ CLICK ACÁ"/>
+            </div>
+            <div className="video-container">
+                <img src="images/mobile/PNG/Ashoka-Landing-Mobile_Imagen-02.png"/>
+            </div>
+        </section>
+        </>
      );
 }
  
