dee44404d3070af7513d8b796e823fcc985c4ea7
JonatanCespedes <retratos.jonatan@gmail.com>
2021-03-21 14:08:04 -0300

    Knowmore section desktop complete

--- src/Components/Know-more/Knowmore.js
+++ src/Components/Know-more/Knowmore.js
@@ -5,11 +5,18 @@ import './Knowmore.css';
 const Knowmore = () => {
     return ( 
         <section className="knowmore-section">
-            <img src="images/mobile/PNG/Ashoka-Landing-Mobile_Imagen-05.png"/>
-            <h6>
-                LA MAYORÍA DE LOS EMPRENDEDORES SOCIALES DE <span className="text-cian">ASHOKA</span> HAN TENIDO SU PRIMERA EXPERIENCIA COMO AGENTES <span className="text-orange">DE CAMBIO</span> ANTES DE LOS 20 AÑOS.
-            </h6>
-            <Button className="btn-orange" text="CONOCÉ MÁS"/>
+            <div className="knowmore__img">
+                <img id="img-mobile" src="images/mobile/PNG/Ashoka-Landing-Mobile_Imagen-05.png"/>
+                <img id="img-tablet" src="images/mobile/PNG/Ashoka-Landing-Mobile_Imagen-05.png"/>
+                <img id="img-desktop" src="images/desktop/PNG/Ashoka-Landing-Escritorio_Imagen-05.png"/>
+            </div>
+            <div className="knowmore__info">
+                <h6>
+                    LA MAYORÍA DE LOS EMPRENDEDORES SOCIALES DE <span className="text-cian">ASHOKA</span> HAN TENIDO SU PRIMERA EXPERIENCIA COMO AGENTES <span className="text-orange">DE CAMBIO</span> ANTES DE LOS 20 AÑOS.
+                </h6>
+                <Button id="knowmore__btn" className="btn-orange" text="CONOCÉ MÁS"/>
+            </div>
+            
         </section>
      );
 }
