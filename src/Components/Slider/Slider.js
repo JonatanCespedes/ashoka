e944e607602826d5a11b5b04e70230f9436d1b70
JonatanCespedes <retratos.jonatan@gmail.com>
2021-03-22 22:05:16 -0300

    Slider stable

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -5,46 +5,58 @@ import './Slider.css'
 
 const Slider = () => {
 
-    const [slide, setSlide] = useState(1)
-
+    const [slide, setSlide] = useState(0);
+ 
     useEffect(() => {
         ((d) => {
             const $btnPrev = d.querySelector("#row-left"),
             $btnNext = d.querySelector("#row-right"),
-            $dot_1 = d.querySelector("#dot-1"),
-            $dot_2 = d.querySelector("#dot-2"),
-            $dot_3 = d.querySelector("#dot-3"),
+            $dot_1 = d.querySelector("#slide-1"),
+            $dot_2 = d.querySelector("#slide-2"),
+            $dot_3 = d.querySelector("#slide-3"),
             $slide1 = d.querySelector(".slide-1"),
             $slide2 = d.querySelector(".slide-2"),
             $slide3 = d.querySelector(".slide-3");
 
-            $btnNext.addEventListener("click", e=>{
-                if(slide == 1){
-                    setSlide(slide + 1)
-                    $slide1.classList.toggle("none");
-                    $slide2.classList.toggle("none");
-                }
-                console.log(slide)
-                if(slide == 2){
-                    setSlide(slide + 1)
-                }
-                if(slide == 3){
-                    $slide2.classList.toggle("none");
-                    $slide3.classList.toggle("none");
-                }
-                console.log(slide)
 
-            });
-        
-           /*  d.addEventListener("click", e=>{
-                if(!e.target.matches(".menu a"))return false;
-        
-                $btnMenu.firstElementChild.classList.remove("none");
-                $btnMenu.lastElementChild.classList.add("none");
-                $menu.classList.remove("is-active");
-            }) */
+            switch (slide) {
+                case -1:
+                    setSlide(2)
+                case 0: 
+                    $slide1.classList.remove('none');
+                    $slide2.classList.add('none');
+                    $slide3.classList.add('none');
+                    $dot_1.checked = true;
+                    $dot_2.checked = false;
+                    $dot_3.checked = false;
+                    break;
+                case 1:
+                    $slide1.classList.add('none');
+                    $slide2.classList.remove('none');
+                    $slide3.classList.add('none');
+                    $dot_1.checked = false;
+                    $dot_2.checked = true;
+                    $dot_3.checked = false;
+                    break;
+                case 2:
+                    $slide1.classList.add('none');
+                    $slide2.classList.add('none');
+                    $slide3.classList.remove('none');
+                    $dot_1.checked = false;
+                    $dot_2.checked = false;
+                    $dot_3.checked = true;
+                    break;
+                case 3:
+                    setSlide(0)
+                    $slide3.classList.add('none');
+                    $slide1.classList.remove('none')
+                default:
+                    break;
+            }
         })(document);
-    }, []);
+    }, [slide]);
+
+    /* e.target.classList.add('fade-out'); */
        
 
     return ( 
@@ -53,7 +65,7 @@ const Slider = () => {
             <input type="radio" name="slides" id="slide-2"  />
             <input type="radio" name="slides" id="slide-3" />
             <div className="slides">
-                <div className="slide-1">
+                <div className="slide-1 fade-in ">
                     <div className="slide-info">
                         <h2 className="main-title">
                             <span className="text-orange">JÓVENES </span><br/> <span className="text-white">QUE TRANSFORMAN</span>
@@ -71,7 +83,7 @@ const Slider = () => {
                         <img id="img-1" src="images/desktop/PNG/Ashoka-Landing-Escritorio_Imagen-01.png"/>
                     </div>
                 </div>
-                <div className="slide-2 none">
+                <div className="slide-2 none fade-in">
                     <div className="slide-info">
                         <h2 className="main-title">
                         <span className="text-orange">SOBRAN UTOPÍAS </span><br/> <span className="text-white">FALTAN REALIDADES</span>
@@ -89,7 +101,7 @@ const Slider = () => {
                         <img src="images/desktop/PNG/Ashoka-Landing-Escritorio_Imagen-06.png"/>
                     </div>
                 </div>
-                <div className="slide-3 none">
+                <div className="slide-3 none fade-in">
                     <div className="slide-info">
                         <h2 className="main-title">
                         <span className="text-orange">NO COMUNES, </span><br/> <span className="text-white">SÍ BIEN COMÚN</span>
@@ -107,13 +119,13 @@ const Slider = () => {
                         <img src="images/desktop/PNG/Ashoka-Landing-Escritorio_Imagen-07.png"/>
                     </div>
                 </div> 
-                <span id="row-left"><i class="fas fa-chevron-left"></i></span>
-                <span id="row-right"><i class="fas fa-chevron-right"></i></span>    
+                <span id="row-left"  onClick={()=>setSlide(slide - 1)}><i  className="fas fa-chevron-left"></i></span>
+                <span id="row-right" onClick={()=>setSlide(slide + 1)}><i className="fas fa-chevron-right"></i></span>    
             </div>
             <aside class="slides-nav">
-                <label for="slide-1" id="dot-1"></label>
-                <label for="slide-2" id="dot-2"></label>
-                <label for="slide-3" id="dot-3"></label>
+                <label htmlFor="slide-1" id="dot-1" onClick={()=>setSlide(0)}></label>
+                <label htmlFor="slide-2" id="dot-2" onClick={()=>setSlide(1)}></label>
+                <label htmlFor="slide-3" id="dot-3" onClick={()=>setSlide(2)}></label>
             </aside>  
               
         </section>
