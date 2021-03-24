2c51123cfa7d49c63794763ca1a2d78093b40c59
Ignacio Romani <promani7@gmail.com>
2021-03-24 18:50:50 -0300

    prueba google form

--- src/Components/Contact-Form/Contact.js
+++ src/Components/Contact-Form/Contact.js
@@ -11,6 +11,11 @@ const Contact = () => {
         Aos.init({duration: 3000}); 
     }, []);
 
+    var reset = () => {
+        //document.getElementById("contact_form").reset();
+        document.getElementById("gracias").style.display = 'block';
+    }
+
     return ( 
         <section id="contacto" className="contact-section">
                 <h3 data-aos="fade" className="text-orange">
@@ -24,12 +29,17 @@ const Contact = () => {
                     <a data-aos="fade" href="mailto:infoargentina@ashoka.org">infoargentina@ashoka.org</a>
                 </div>
                 <div className="contact__form">
-                    <form autoComplete="off" className="form-container">
-                        <Input inputName="Nombre" id="inputName" type="text" name="name" className="input-white"/>
-                        <Input inputName="Email" id="inputEmail" type="email" name="email" className="input-white"/>
-                        <Input inputName="Celular" id="inputCel" type="text" name="cel" className="input-white"/>
-                        <textarea placeholder="Consulta" name="message" id="" cols="30" rows="10"></textarea>
-                        <Button className="btn-white" text="ENVIAR"/>
+                    <iframe name="hidden_iframe" id="hidden_iframe" style={{display:'none'}}
+                            onLoad="if(submitted) {window.location='thankyou.html';}"></iframe>
+                    <p id="gracias" style={{display:'none'}}><b>Gracias por contactarte</b></p>
+                    <form autoComplete="off" className="form-container" id={'contact_form'}
+                          action="https://docs.google.com/forms/d/e/1FAIpQLSd054WJx5e6-VB8RWb-O-ntOrUkqunTDRVC-rLOMHzpuFahgg/formResponse"
+                          target="hidden_iframe"  method="post" onSubmit={reset}>
+                        <Input inputName="Nombre" id="inputName" type="text" name="entry.1924373565" className="input-white"/>
+                        <Input inputName="Email" id="inputEmail" type="email" name="entry.993016979" className="input-white"/>
+                        <Input inputName="Celular" id="inputCel" type="text" name="entry.890105707" className="input-white"/>
+                        <textarea placeholder="Consulta" name="entry.412920707" id="" cols="30" rows="10"></textarea>
+                        <Button className="btn-white" text="ENVIAR" onClick={reset}/>
                     </form>
                 </div>
             </div>        
